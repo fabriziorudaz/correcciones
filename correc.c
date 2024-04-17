082ab73836969322fb40dd601fdbf36b049b2012
fabriziorudaz <130599313+fabriziorudaz@users.noreply.github.com>
2024-04-17 12:34:28 -0300

    Update correc.c

--- correc.c
+++ correc.c
@@ -1,86 +1,103 @@
-#include<stdio.h>
-#include<stdio.h>
+#include <stdio.h>
+#include<stdlib.h>
 
 int main(){
 int opcion;
+puts("1 estacionamiento");
+puts("2 multiplicacion");
+puts("3 recorrido");
+puts("4 entradas");
+puts("5 comida");
 scanf("%d", &opcion);
 
 switch(opcion){
 	case 1:{
-		int hora_e, hora_s;
+   		int hora_e, hora_s;
 		scanf("%d %d", &hora_e, &hora_s);
 		int costo = 500;
 		if(hora_s - hora_e <= 100){
 			printf("Tu costo es de %d", costo);
 		}
-		else if(hora_s - hora_e >= 100) {
-			int costo2;
-			for (int i = 0; i < hora_s-hora_e; i+=250){
-			costo2 += i;
+		else if(hora_s - hora_e > 100) {
+			int costo2 = 250;
+            int horas_estadia = (hora_s - hora_e)/100;
+            printf("tu costo es de %d", costo + costo2*horas_estadia);
+		
 		}
-		printf("tu costo es de %d ", costo + costo2);
 		return 0;
 		}
 		break;
-	}
 	case 2:{
+		
 		int num1, num2;
 		
-		scanf("%f %f", &num1, &num2);
-		if(num1 <= 0 || num2 <= 0){
+		scanf("%d %d", &num1, &num2);
+		if(num1 < 0 || num2 < 0){
 			printf("un numero es negativo, error");
 		}
-			float resultado = num1 += num2;
-			printf("%f", resultado);
+        else{
+			int resultado = 0;
+            int i = 0;
+            while(i < num1){
+                i++;
+                resultado += num2;
+           	 }
+			printf("%d", resultado);
 			return 0;
-		
+		}
+    
 		break;
 	}
 	case 3:{
-		float km, horas, minutos, precio_litro;
+		 float km, horas, minutos, precio_litro;
 		
-		scanf("%f %f %f", &km, &horas, &minutos, &precio_litro);
-		float velocidad= km / horas+(minutos/60);
+		scanf("%f %f %f %f", &km, &horas, &minutos, &precio_litro);
+		float velocidad = km / horas+(minutos/60);
 			float litros_100 = 100 / velocidad;
 			float pesos_100 = precio_litro * (100/ velocidad);
 			float pesos = precio_litro*(1/velocidad);
 			float litro = 1/velocidad;
 			
-			printf("por 100km se gasta %.2f$ y se gastaun total de %.2f litros\n", pesos_100, litros_100);
-			printf("por km se gasta %.2f& y se gasta un total de %.2f litros", pesos, litro);
+			printf("por 100km se gasta %.2f$ y se gasta un total de %.2f litros\n", pesos_100, litros_100);
+			printf("por km se gasta %.2f$ y se gasta un total de %.2f litros", pesos, litro);
 			return 0;
 		
 		break;
-	}
+		}
 	case 4:{
-		int entrada, precio;
+		 int entrada, precio;
 		scanf("%d %d", &entrada, &precio);
-		if(entrada == 2){
-		printf("el costo es igual a %d", precio*2+(precio*0.1));
+		if (entrada == 1){	
+        		printf("su entrada cuesta %d", precio);
+		}
+		else if(entrada == 2){
+			printf("el costo es igual a %.f", precio*2-((precio*2)*0.1));
 		} else if (entrada == 3){
-			printf("el costo es de %d", precio*3+(precio*0.15));
+			printf("el costo es de %.f", precio*3-((precio*3)*0.15));
 		}else if(entrada == 4){
-			printf("el costo es de %d", precio*4+(precio*0.20));
+			printf("el costo es de %.f", precio*4-((precio*4)*0.20));
 		}else if(entrada > 4){
-			printf("no sepuede comprar");
+			printf("no se puede comprar");
 		}
 		
 		return 0;
 		}
 		break;
-		case 5:{
+	case 5:{
 			int persona;
-			scanf("%d %d", persona);
+			scanf("%d", &persona);
 			
 			if(persona == 1){
 				printf("el precio del plato es de 6500");
-			}else if(persona >= 200 && persona <= 300){
+			}else if(persona >= 200 && persona < 300){
 				printf("el costo del plato es de 5500");
-			}else if(persona >= 4500){
+			}else if(persona >= 300){
 				printf("el costo del plato es de 4500");
 			}
-			break;
+			return 0;
+
+            break;
+			}
 		}
-	}
-	return 0;
+	return 0;		
 }
